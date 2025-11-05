f6591ad4b3a3cdd45f5fc59b47228bb65396ccdf
Kblack (Solo dev) <65351458+khoa083@users.noreply.github.com>
2025-11-05 21:35:08 +0700

    upd base repository

--- app/src/main/java/com/kblack/project_base/base/BaseRepository.kt
+++ app/src/main/java/com/kblack/project_base/base/BaseRepository.kt
@@ -1,8 +1,6 @@
 package com.kblack.project_base.base
 
-import android.content.Context
 import com.kblack.project_base.base.network.NetworkMonitor
-import com.kblack.project_base.base.network.NetworkMonitorImpl
 import com.kblack.project_base.utils.DataResult
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
@@ -15,7 +13,6 @@ import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 import java.io.IOException
 import java.sql.SQLException
-import java.util.concurrent.ConcurrentHashMap
 
 /**
  * BaseRepository provides a standardized way to handle data operations.
@@ -23,7 +20,8 @@ import java.util.concurrent.ConcurrentHashMap
  */
 abstract class BaseRepository(
     protected val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
-    private val cache: MutableMap<String, Any?> = ConcurrentHashMap(),
+    private val cache: MutableMap<String, Any?> = HashMap(), // Simple in-memory cache HashMap, LinkedHashMap for LRU (Least Recently Used) cache
+    private val networkMonitor: NetworkMonitor,
 ) {
 
     /**
@@ -37,30 +35,25 @@ abstract class BaseRepository(
         call: suspend () -> T,
         cacheKey: String? = null,
         onError: (Throwable) -> Unit = {},
-        context: Context
     ): Flow<DataResult<T>> = flow {
         emit(DataResult.loading())
 
-        if (!isNetworkAvailable(context)) {
-            val error = IOException("No network connection")
+        if (!networkMonitor.isNetworkConnected()) {
+            val error = IOException("[executeNetworkCall] No network connection")
             onError(error)
-            emit(DataResult.error("No network connection"))
+            emit(DataResult.error(error.message ?: "No network"))
             return@flow
         }
 
         try {
-            val result = cacheKey?.let { key ->
-                @Suppress("UNCHECKED_CAST")
-                cache[key] as? T
-            } ?: run {
-                val data: T = call()
-                cacheKey?.let { key -> cache[key] = data }
-                data
-            }
+            val result = getOrFetchFromCache(cacheKey, call)
             emit(DataResult.success(result))
         } catch (e: HttpException) {
             onError(e)
-            emit(DataResult.error("No network connection"))
+            emit(DataResult.error(e.message ?: "HTTP Error"))
+        } catch (e: Exception) {
+            onError(e)
+            emit(DataResult.error(e.message ?: "Unknown error"))
         }
     }.flowOn(ioDispatcher)
 
@@ -69,33 +62,42 @@ abstract class BaseRepository(
      * @param call Suspend function representing the database operation.
      * @return Result with Success or Error state.
      */
-    protected suspend fun <T> executeLocalCall(call: suspend () -> T): DataResult<T> {
-        return try {
-            withContext(ioDispatcher) {
+    protected suspend fun <T> executeLocalCall(call: suspend () -> T): DataResult<T> =
+        withContext(ioDispatcher) {
+            try {
                 DataResult.success(call())
+            } catch (e: SQLException) {
+                DataResult.error("Database error [executeLocalCall]: ${e.message}")
+            } catch (e: Exception) {
+                DataResult.error("Unknown error [executeLocalCall]: ${e.message}")
             }
-        } catch (e: SQLException) {
-            DataResult.error("Database error")
-        } catch (e: Exception) {
-            DataResult.error("Unknown error")
         }
-    }
 
     /**
      * Wraps a Flow from a local data source with error handling.
      * @param flowCall Flow representing the local data source.
      * @return Flow emitting Result with Success or Error states.
      */
-    protected fun <T> wrapLocalFlow(flowCall: Flow<T>): Flow<DataResult<T>> {
-        return flowCall
+    protected fun <T> wrapLocalFlow(flowCall: Flow<T>): Flow<DataResult<T>> =
+        flowCall
             .map { DataResult.success(it) }
-            .catch {}
+            .catch { emit(DataResult.error("[ERROR:wrapLocalFlow] ${it.message}" ?: "Unknown error [wrapLocalFlow]")) }
             .flowOn(ioDispatcher)
-    }
 
-    private suspend fun isNetworkAvailable(context: Context): Boolean {
-        val networkMonitor: NetworkMonitor = NetworkMonitorImpl(context)
 
-        return networkMonitor.isNetworkConnected()
-    }
+    // Check network availability
+//    private suspend fun isNetworkAvailable(context: Context): Boolean
+//        = NetworkMonitorImpl(context).isNetworkConnected()
+
+    // Get from cache or fetch and cache the result
+    @Suppress("UNCHECKED_CAST")
+    private suspend fun <T> getOrFetchFromCache(
+        cacheKey: String?,
+        call: suspend () -> T,
+    ): T = cacheKey?.let { key ->
+        (cache[key] as? T) ?: fetchAndCache(key, call)
+    } ?: call()
+
+    private suspend fun <T> fetchAndCache(key: String, call: suspend () -> T): T =
+        call().also { cache[key] = it }
 }
